1872e4604b2f4200b9e8e766eda204c5db1df0da
tianwenyuan <tianwenyuan@suitbim.com>
2024-09-18 16:36:06 +0800

    5.5.0版本和jpa无法兼容

--- src/main/java/com/demo/shardingsphere/core/Person.java
+++ src/main/java/com/demo/shardingsphere/core/Person.java
@@ -1,13 +1,11 @@
 package com.demo.shardingsphere.core;
 
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Transient;
 
 /**
  * Created with IntelliJ IDEA.
@@ -26,8 +24,6 @@ public class Person {
     private Long id;
     private String name;
     private int age;
-//    @Transient
-//    @TableField(exist = false)
     private String idCard;
     private String telephone;
     private String username;
